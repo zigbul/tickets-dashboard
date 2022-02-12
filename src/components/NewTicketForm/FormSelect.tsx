61d69c1bca7af93de8613dd7cb3c4d79993e7430
Zigbul <zigbul@gmail.com>
2022-02-12 19:38:58 +0300

    FormSelect migrated

--- src/components/NewTicketForm/FormSelect.tsx
+++ src/components/NewTicketForm/FormSelect.tsx
@@ -1,11 +1,18 @@
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
+import { TextFieldProps } from 'material-ui';
 
-const FormSelect = forwardRef((props, ref) => {
+type Props = {
+    value: string,
+    onChange: any & TextFieldProps,
+    name: string,
+}
+
+const FormSelect = forwardRef((props: Props, ref: any & TextFieldProps) => {
 
     return (
         <FormControl required sx={{ m: 1, minWidth: 300 }}>
